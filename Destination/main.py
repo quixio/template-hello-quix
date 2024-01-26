5e7b7ab39cb1abbe07d2906f6895efe7b57079f2
Steve Rosam <srosam+quix-demos@gmail.com>
2024-01-26 14:47:01 +0000

    Edited 'main.py'

--- Destination/main.py
+++ Destination/main.py
@@ -8,10 +8,15 @@ input_topic = app.topic(os.environ["input"], value_deserializer=JSONDeserializer
 
 sdf = app.dataframe(input_topic)
 
-# write code to publish your data to any destination
-# use any Python library you like!
+def publish_to_destination(row: dict):
+    # write code to publish your data to any destination
+    # use any Python library you like!
 
-sdf = sdf.update(lambda row: print(row))
+    print("Publish this data to your destination\nWrite any Python code you need and use any Python library you fancy!")
+
+    pass
+
+sdf = sdf.apply(publish_to_destination)
 
 if __name__ == "__main__":
     app.run(sdf)
