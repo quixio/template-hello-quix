29b964625a70842c517e640a7c37db70cf11cca4
Steve <100689438+SteveRosam@users.noreply.github.com>
2024-01-26 14:57:31 +0000

    Merge to main (#1)
    Initial version of Hello Quix pipeline

---
+++ Destination/main.py
@@ -0,0 +1,32 @@
+import os
+from quixstreams import Application
+from quixstreams.models.serializers.quix import JSONDeserializer
+
+app = Application.Quix("destination-v1", auto_offset_reset="latest")
+
+input_topic = app.topic(os.environ["input"], value_deserializer=JSONDeserializer())
+
+sdf = app.dataframe(input_topic)
+
+print(" ")
+print(" ")
+print(" ")
+print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
+print("Publish this data to your destination\nWrite any Python code you need and use any Python library you fancy!")
+print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
+print(" ")
+print(" ")
+print(" ")
+
+
+def publish_to_destination(row: dict):
+    # write code to publish your data to any destination
+    # use any Python library you like!
+    print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
+    print(f"This is one row of your data\nTransform it here or publish it to an external data store\n{row}")
+    print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
+
+sdf = sdf.apply(publish_to_destination)
+
+if __name__ == "__main__":
+    app.run(sdf)
