f9a71f6f1060d944da45722f4901476d5bfa77f3
Steve Rosam <srosam+quix-demos@gmail.com>
2024-01-26 14:55:32 +0000

    Edited 'main.py'

--- Destination/main.py
+++ Destination/main.py
@@ -23,7 +23,7 @@ def publish_to_destination(row: dict):
     # write code to publish your data to any destination
     # use any Python library you like!
 
-    print(row)
+    print(f"This is one row of your data\nTransform it here or publish it to an external data store\n{row}")
 
 sdf = sdf.apply(publish_to_destination)
 
