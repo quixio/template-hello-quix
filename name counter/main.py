04d4e53f245598af72f747edb6408c8c8c30d62d
SteveRosam <steve@quix.io>
2024-01-31 16:51:18 +0000

    fix issues with missing dot-env package

--- name counter/main.py
+++ name counter/main.py
@@ -2,8 +2,18 @@ import os
 from quixstreams import Application, State
 from quixstreams.models.serializers.quix import JSONDeserializer, JSONSerializer
 
+# import our get_app function to help with building the app for local/Quix deployed code
+from app_factory import get_app
 
-app = Application.Quix("transformation-v1", auto_offset_reset="latest")
+# import the dotenv module to load environment variables from a file
+from dotenv import load_dotenv
+load_dotenv(override=False)
+
+# get the environment variable value or default to False
+USE_LOCAL_KAFKA=os.getenv("use_local_kafka", False)
+
+# Create an Application.
+app = get_app(use_local_kafka=USE_LOCAL_KAFKA)
 
 input_topic = app.topic(os.environ["input"], value_deserializer=JSONDeserializer())
 output_topic = app.topic(os.environ["output"], value_serializer=JSONSerializer())
