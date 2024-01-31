71a09357cc5b393c99b1c87cff8762e10b339c5a
Steve Rosam <srosam+quix-demos@gmail.com>
2024-01-31 14:06:32 +0000

    Edited 'main.py'

--- csv data source/main.py
+++ csv data source/main.py
@@ -1,7 +1,7 @@
 # Import the Quix Streams modules for interacting with Kafka: 
 from quixstreams.models.serializers.quix import JSONSerializer, SerializationContext
 # (see https://quix.io/docs/quix-streams/v2-0-latest/api-reference/quixstreams.html for more details)
-
+ 
 # Import additional modules as needed
 import pandas as pd
 import random
