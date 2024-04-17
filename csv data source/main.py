3c2aa2271b41e80bb5f5d536f01c22be89b97fd8
Kris Jenkins <krisajenkins@gmail.com>
2024-04-17 14:19:02 +0100

    Improved the use of `with` in the CSV data source code.

--- csv data source/main.py
+++ csv data source/main.py
@@ -85,9 +85,8 @@ def main():
     # Producer is already setup to use Quix brokers.
     # It will also ensure that the topics exist before producing to them if
     # Application.Quix is initiliazed with "auto_create_topics=True".
-    producer = app.get_producer()
 
-    with producer:
+    with app.get_producer() as producer:
         # Iterate over the data from CSV file
         for message_key, row_data in read_csv_file(file_path=csv_file_path):
             json_data = json.dumps(row_data)
