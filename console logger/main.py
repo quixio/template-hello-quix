0ea80fa20e4990e903f5660790138fcbbe619b58
Kris Jenkins <krisajenkins@gmail.com>
2024-04-17 14:16:56 +0100

    Splitting some `print("...\n...")` lines into separate print statements.
    Small beer, but it's just that bit easier to understand when the output
    you see matches the source code closely.

--- console logger/main.py
+++ console logger/main.py
@@ -18,7 +18,8 @@ print(" ")
 print(" ")
 print(" ")
 print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
-print("Publish this data to your destination\nWrite any Python code you need and use any Python library you fancy!")
+print("Publish this data to your destination")
+print("Write any Python code you need and use any Python library you fancy!")
 print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
 print(" ")
 print(" ")
@@ -29,7 +30,9 @@ def publish_to_destination(row: dict):
     # write code to publish your data to any destination
     # use any Python library you like!
     print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
-    print(f"This is one row of your data\nTransform it here or publish it to an external data store\n{row}")
+    print("This is one row of your data")
+    print("Transform it here or publish it to an external data store")
+    print(row)
     print("-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~-~")
 
 sdf = sdf.apply(publish_to_destination)
